286d04258ec7b6613a1a8e53959b5337486c22e4
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-12-29 13:35:45 +0800

    Bump version 1.8.0 -> 1.8.1

--- sfzen/__init__.py
+++ sfzen/__init__.py
@@ -61,7 +61,7 @@ __all__ = [	'COMMENT_DIVIDER',
 			'midi_note_sort_key', 'sorted_opcode_names', 'sorted_opcodes', 'sorted_opstrings',
 			'opcodes_to_dict', 'opstrings_to_dict']
 
-__version__ = "1.8.0"
+__version__ = "1.8.1"
 
 COMMENT_DIVIDER = '// ' + '-' * 76 + "\n"
 
